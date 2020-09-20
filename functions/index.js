3491cae7ec70e7468e02a3064dbf53e055ab7772
Takuma Tachibana <moja0316.takuma@gmail.com>
2020-09-20 14:49:06 +0900

    add content inside cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,7 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.use('/content', express.static(__dirname + '/content'))
+
+exports.loadContents = functions.https.onRequest(app);
